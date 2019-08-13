12ab666b0341fc6146e332cdb64c59fbab6f0ef5
Luis <jluisp.m.19@ciencias.unam.mx>
2019-08-13 11:29:28 -0500

    Prueba

---
+++ Main.java
@@ -0,0 +1,8 @@
+
+public class Main{
+	public static void main(String[]args){
+    System.out.println("openjdk version '11.0.4' 2019-07-16");
+    System.out.println("OpenDK Runtime Environment (build 11.0.4+11-post-Ubuntu-1ubuntu218.04.3)");
+    System.out.println("OpenJDK 64-Bit Server VM (build 11.0.4+11-post-Ubuntu-1ubuntu218.04.3, mixed mode, sharing)");
+	}
+}
